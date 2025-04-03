c919ea15f7d0b4929fb1b21c7c1342e8254c99b8
Combir <144319259+Combir@users.noreply.github.com>
2025-04-03 12:28:49 +0300

    adding lab

---
+++ GameForLesson4/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource audioSource;
+    private bool isPicked = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TriggerPickupAnimation()
+    {
+        if (!isPicked)
+        {
+            isPicked = true;
+
+            anim.SetTrigger("pickupTrigger");
+
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.PlayOneShot(audioSource.clip);
+            }
+
+
+            Invoke("DestroySelf", 0.5f); 
+        }
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !isPicked) 
+        {
+            TriggerPickupAnimation();
+        }
+    }
+}
